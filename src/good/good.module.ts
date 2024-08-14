c09e1a5611055451d6e4d33b5181b0fe7971b9df
snarmatov <snarmatov@domrf.ru>
2024-08-14 17:10:57 +0300

    update modules

--- src/good/good.module.ts
+++ src/good/good.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GoodService } from './good.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { GoodController } from './good.controller';
 
 @Module({
   providers: [GoodService, PrismaService],
+  controllers: [GoodController],
 })
 export class GoodModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { User as UserModel } from '.prisma/client';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  private readonly userService: UserService;
+
+  @Post()
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
