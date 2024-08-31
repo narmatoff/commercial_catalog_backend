cb75233b057ad3d04fad7201adb89ae7a9182eb2
snarmatov <snarmatov@domrf.ru>
2024-09-01 02:50:47 +0300

    add migration

---
+++ prisma/migrations/20240831235027_init/migration.sql
@@ -0,0 +1,104 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "telegramId" INTEGER NOT NULL,
+    "email" TEXT,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "prodId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "vendorCode" TEXT NOT NULL,
+    "vendorId" INTEGER NOT NULL,
+    "infoPrice" DOUBLE PRECISION NOT NULL,
+    "description" TEXT NOT NULL,
+    "batteries" TEXT,
+    "pack" TEXT NOT NULL,
+    "material" TEXT NOT NULL,
+    "length" DOUBLE PRECISION,
+    "diameter" DOUBLE PRECISION,
+    "collection" TEXT,
+    "img1" TEXT,
+    "img2" TEXT,
+    "img3" TEXT,
+    "img4" TEXT,
+    "img5" TEXT,
+    "img6" TEXT,
+    "img7" TEXT,
+    "img8" TEXT,
+    "img9" TEXT,
+    "img10" TEXT,
+    "categoryId" INTEGER NOT NULL,
+    "isNew" BOOLEAN NOT NULL,
+    "isBestseller" BOOLEAN NOT NULL,
+    "function" TEXT,
+    "addFunction" TEXT,
+    "vibration" TEXT,
+    "volume" TEXT,
+    "modelYear" INTEGER,
+    "imgStatus" BOOLEAN NOT NULL,
+    "lefts" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "categoryId" INTEGER,
+    "parentId" INTEGER,
+    "name" TEXT NOT NULL,
+    "sort" INTEGER NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Basket" (
+    "id" SERIAL NOT NULL,
+    "telegramId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Basket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BasketItem" (
+    "id" SERIAL NOT NULL,
+    "basketId" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BasketItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_telegramId_key" ON "User"("telegramId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_prodId_key" ON "Product"("prodId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Basket_telegramId_key" ON "Basket"("telegramId");
+
+-- AddForeignKey
+ALTER TABLE "Basket" ADD CONSTRAINT "Basket_telegramId_fkey" FOREIGN KEY ("telegramId") REFERENCES "User"("telegramId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BasketItem" ADD CONSTRAINT "BasketItem_basketId_fkey" FOREIGN KEY ("basketId") REFERENCES "Basket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BasketItem" ADD CONSTRAINT "BasketItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
