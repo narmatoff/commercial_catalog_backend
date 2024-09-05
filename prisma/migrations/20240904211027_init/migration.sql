88bc6c1b732a60b1ca2d956c772b73360217c592
snarmatov <snarmatov@domrf.ru>
2024-09-05 10:55:01 +0300

    improve offers import

---
+++ prisma/migrations/20240904211027_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ProductOffers" ALTER COLUMN "barcode" SET DATA TYPE TEXT;
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -122,24 +122,24 @@ export class ProductService {
         .on('data', async (row) => {
           const offer = {
             prodId: parseInt(row.prodid),
-            sku: row.sku,
+            sku: parseInt(row.sku),
             barcode: row.barcode,
             name: row.name.replace(/"/g, ''),
             price: parseFloat(row.price),
             WholePrice: parseFloat(row.WholePrice),
-            baseWholePrice: parseFloat(row.basewholeprice),
+            basewholeprice: parseFloat(row.basewholeprice),
             qty: parseInt(row.qty),
-            shippingDate: row.shippingdate,
-            weight: row.weight,
+            shippingdate: row.shippingdate || null,
+            weight: parseFloat(row.weight) || null,
             colorName: row.colorName,
             color: row.color,
-            size: row.size,
-            SuperSale: Boolean(parseInt(row.SuperSale)),
+            size: row.size || null,
+            SuperSale: Boolean(parseInt(row.SuperSale)) || null,
             p5s_stock: Boolean(parseInt(row.p5s_stock)),
             StopPromo: Boolean(parseInt(row.StopPromo)),
-            bruttoLength: parseFloat(row.bruttoLength),
-            bruttoWidth: parseFloat(row.bruttoWidth),
-            bruttoHeight: parseFloat(row.bruttoHeight),
+            bruttoLength: parseFloat(row.bruttoLength) || null,
+            bruttoWidth: parseFloat(row.bruttoWidth) || null,
+            bruttoHeight: parseFloat(row.bruttoHeight) || null,
             currency: row.currency,
           };
 
@@ -148,6 +148,13 @@ export class ProductService {
         .on('end', async () => {
           // Сохраняем продукты в базу данных
           for (const offer of offers) {
+            // const product = await this.prisma.product.findUnique({
+            //   where: { prodId: offer.prodId },
+            // });
+
+            // if (!product) {
+            //   continue;
+            // }
             await this.prisma.productOffers.upsert({
               // TODO доделать
               where: { prodId: offer.prodId },
