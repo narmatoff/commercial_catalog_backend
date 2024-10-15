eb578f0d5c762b1af7090423f2a963a6341c9d5e
snarmatov <snarmatov@domrf.ru>
2024-10-16 02:21:18 +0300

    upd CreateUserDto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,17 +1,3 @@
-import { IsNotEmpty, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends CreateUserDto {
-  // TODO: вернуть после ресерча (валидация в тлг)
-  // @IsEmail()
-  @IsNotEmpty()
-  email: string;
-
-  @IsString()
-  fio: string;
-
-  // TODO: вернуть после ресерча (валидация в тлг)
-  // @IsPhoneNumber()
-  @IsNotEmpty()
-  phone: string;
-}
+export class UpdateUserDto extends CreateUserDto {}
