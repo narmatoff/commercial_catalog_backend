99f3bb5395211612d403dbbe2c8ff7a5c36ff57e
snarmatov <snarmatov@domrf.ru>
2024-12-17 23:00:13 +0300

    - fix creating user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // ping
   @Get('ping')
   getPong(): string {
     return this.appService.getPong();
